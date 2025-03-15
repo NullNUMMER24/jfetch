46880fa8b3b8317fab218be6f7c9640e37b74bb6
NullNUMMER24 <j.rohrbach@sensemail.ch>
2025-03-15 21:26:34 +0100

    replaced bash command with host lib

--- go.mod
+++ go.mod
@@ -1,5 +1,12 @@
 module jfetch
 
-go 1.22.8
+go 1.23.0
+
+toolchain go1.23.5
 
 require github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
+
+require (
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	golang.org/x/sys v0.31.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 	"os"
 	"os/exec"
 	"runtime"
 	"strings"
+	"time"
 
 	"github.com/common-nighthawk/go-figure"
+	"github.com/shirou/gopsutil/host"
 )
 
 func getOS() string {
@@ -35,21 +39,26 @@ func getKernelVersion() (string, error) {
 	return strings.TrimSpace(string(output)), nil
 }
 
-func getUptime() (string, error) {
-	var cmd *exec.Cmd
-	switch runtime.GOOS {
-	case "linux", "darwin":
-		cmd = exec.Command("bash", "-c", "uptime | sed 's/.*up \\([^,]*\\), .*/\\1/'")
-	case "windows":
-		cmd = exec.Command("net", "stats", "srv") // This is a workaround for Windows
-	default:
-		return "Uptime not available", nil
-	}
-	output, err := cmd.Output()
+func formatUptime(seconds uint64) string {
+	duration := time.Duration(seconds) * time.Second
+	days := duration / (24 * time.Hour)
+	duration -= days * 24 * time.Hour
+	hours := duration / time.Hour
+	duration -= hours * time.Hour
+	minutes := duration / time.Minute
+	return fmt.Sprintf("%d days, %d hours, %d minutes", days, hours, minutes)
+}
+
+func GetOutboundIP() (net.IP, error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
-		return "", err
+		log.Fatal(err)
 	}
-	return strings.TrimSpace(string(output)), nil
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP, err
 }
 
 func main() {
@@ -65,9 +74,15 @@ func main() {
 		fmt.Println("Error getting kernel version:", err)
 		return
 	}
-	uptime, err := getUptime()
+	uptimeSeconds, err := host.Uptime()
 	if err != nil {
 		fmt.Println("Error getting uptime:", err)
+		return
+	}
+	uptime := formatUptime(uptimeSeconds)
+	ipAddress, err := GetOutboundIP()
+	if err != nil {
+		fmt.Println("Error getting IP address:", err)
 		return
 	}
 
@@ -76,9 +91,10 @@ func main() {
 	myFigure.Print()
 	fmt.Printf("\033[0m")
 
-	fmt.Printf("OS: %s\n", strings.Title(osName))
+	fmt.Printf("\nOS: %s\n", strings.Title(osName))
 	fmt.Printf("Host: %s\n", hostname)
 	fmt.Printf("User:  %s\n", user)
 	fmt.Printf("Kernel Version: %s\n", kernelVersion)
 	fmt.Printf("Uptime: %s\n", uptime)
+	fmt.Printf("IP Address: %s\n", ipAddress)
 }
